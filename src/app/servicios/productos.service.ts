bcb2581d215a16bf953c047200c32e48e4fbdb01
Gabriel F <drogychanel@gmail.com>
2024-09-12 13:49:54 -0600

    casaitos

--- src/app/servicios/productos.service.ts
+++ src/app/servicios/productos.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  private apiUrl = 'https://sgacineteca-production.up.railway.app/api/productos';
+  private apiUrl = 'https://sgacineteca-production.up.railway.app/api/productos'; // Concatenado con api/productos
 
   constructor(private http: HttpClient) { }
 
   getProductos(): Observable<any> {
-    return this.http.get(this.apiUrl);
+    const headers = new HttpHeaders({ 'Cache-Control': 'no-cache' });
+    return this.http.get(this.apiUrl, { headers });
   }
 }
